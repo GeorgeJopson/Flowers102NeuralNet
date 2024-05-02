4235153ce0cdfb3c39c8aa8d23ddd608c9c0f519
George Jopson <64833916+GeorgeJopson@users.noreply.github.com>
2024-05-02 19:34:37 +0100

    Loaded in data

--- neuralNet.py
+++ neuralNet.py
@@ -2,7 +2,39 @@ import torch
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import ToTensor
+import torchvision.transforms as transforms
+import scipy
 import time
-print("Neural Net code to go here")
+
+# The average height x width of each image is 540x630 so I am going to resize all images
+# to those dimensions
+transformation = transforms.Compose([
+  transforms.Resize((540,620)),
+  transforms.ToTensor()
+])
+
+print("Download training data")
+training_data = datasets.Flowers102(
+  root="data",
+  split="train",
+  download=True,
+  transform=transformation
+)
+print("Finished downloading training data")
+
+print("Downloading test data")
+test_data = datasets.Flowers102(
+  root="data",
+  split="test",
+  download=True,
+  transform=transformation
+)
+print("Finished downloading test data")
+
+batch_size = 64
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data,batch_size=batch_size)
+
 print("Done!")
